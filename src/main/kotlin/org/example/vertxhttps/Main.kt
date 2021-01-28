98fd30bef651c8f4444e2601550a9f6c9c128ea7
happylynx <happylynx@localhost>
2021-01-28 01:34:21 +0100

    Working https demo

--- src/main/kotlin/org/example/vertxhttps/Main.kt
+++ src/main/kotlin/org/example/vertxhttps/Main.kt
@@ -2,16 +2,28 @@ package org.example.vertxhttps
 
 import io.vertx.core.Vertx
 import io.vertx.core.http.HttpServerOptions
+import io.vertx.core.net.JksOptions
+import java.nio.file.Path
 
 class Main {
     companion object {
+
+        private fun readKeystorePassword(keystorePath: String): String {
+            println("Enter keystore (keystorePath) password:")
+            return System.`in`.reader().buffered().readLine().trim()
+        }
+
         @JvmStatic
         fun main(args: Array<String>) {
+            val keystorePath = Path.of("keystore.jks").toAbsolutePath().toString()
+            val keystorePassword = readKeystorePassword(keystorePath)
             val options = HttpServerOptions()
                 .setSsl(true)
+                .setKeyStoreOptions(JksOptions().setPath(keystorePath).setPassword(keystorePassword))
+            val httpsPort: Int = 443
             val serverFuture = Vertx.vertx().createHttpServer(options)
                 .requestHandler { request -> request.response().end("hello") }
-                .listen()
+                .listen(httpsPort)
             serverFuture
                 .onSuccess { server -> println("Server is listening at port ${server.actualPort()}") }
                 .onFailure { error ->
